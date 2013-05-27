210515ec02d450e33f818770033c4bbeae69cf75
Yorick Terweijden <yorick.terweijden@sourcefabric.org>
2013-05-27 13:12:40 +0200

    Clone works

--- package.sh
+++ package.sh
@@ -12,12 +12,20 @@ METHOD=''
 DEFAULT_COMMIT='master'
 COMMIT=''
 
+TARGET_DIR='newscoop_packaging'
+
 exit_usage() {
-    echo "Usage:"
+    echo -e "------------------------------------------------------------"
+    echo -e "Sourcefabric Newscoop Packaging script"
+    echo -e "Usage"
+    echo -e "------------------------------------------------------------"
     echo -e "   -h shows this usage"
     echo -e "   -f FORMAT"
     echo -e "      The output format, can be either ZIP or TAR"
     echo -e "      Defaults to ZIP"
+    echo -e "   -d TARGET_DIR"
+    echo -e "      The Git clone TARGET_DIR"
+    echo -e "      Defaults to newscoop_packaging"
     echo -e "------------------------------------------------------------"
     echo -e "Git checkout method"
     echo -e "Only one of the following can be used."
@@ -29,11 +37,13 @@ exit_usage() {
     echo -e "      Defines which GIT BRANCH should be packaged"
     echo -e "   -t GIT_TAG"
     echo -e "      Defines which GIT TAG should be packaged"
+    echo -e ""
+    echo -e "If none is specified it defaults to BRANCH [master]"
     exit 1
 }
 
 # Loop through all the options and set the vars
-while getopts ":hc:b:f:t:" opt; do
+while getopts ":hc:b:f:t:d:" opt; do
     case $opt in
         h)
             exit_usage
@@ -90,6 +100,9 @@ while getopts ":hc:b:f:t:" opt; do
                 exit_usage
             fi
             ;;
+        d)
+            TARGET_DIR=$OPTARG
+            ;;
         \?)
             echo "Invalid option: -$OPTARG" >&2
             exit_usage
@@ -102,9 +115,28 @@ while getopts ":hc:b:f:t:" opt; do
     esac
 done
 
-if [ -z "$COMMIT" ]; then
+if [ -z $COMMIT ]; then
     COMMIT=$DEFAULT_COMMIT
     METHOD="BRANCH"
 fi
 
-echo "Git $METHOD specified: $COMMIT"
+TARGET_DIR_GIT=$TARGET_DIR
+TARGET_DIR_GIT+='/.git'
+
+if [ ! -d $TARGET_DIR ]; then
+    echo "Git $METHOD specified: $COMMIT"
+    git clone $REPO $TARGET_DIR
+else
+    if [ -d $TARGET_DIR_GIT ]; then
+        echo "Git $METHOD specified: $COMMIT"
+        echo "Git repo already cloned... checking"
+        pushd $TARGET_DIR 1> /dev/null
+        git pull origin
+        popd 1> /dev/null
+    else
+        echo "Directory $TARGET_DIR exists, please remove or specify a different one with:"
+        echo -e "   -d TARGET_DIR"
+        echo -e ""
+        exit_usage
+    fi
+fi
